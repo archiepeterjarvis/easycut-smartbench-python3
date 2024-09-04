5e3c898f84af9af8ef86c1dbf82f14c7757198d9
Archie Jarvis <archiepeterjarvis@gmail.com>
2024-09-04 17:01:31 +0100

    Test with no serial

--- src/serial_kivy_responsiveness.py
+++ src/serial_kivy_responsiveness.py
@@ -1,6 +1,7 @@
 import sys
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
@@ -10,35 +11,34 @@ from core.serial.smartbench_controller import SmartBenchController
 
 class StatusLabel(Label):
 
-    def __init__(self, controller, **kwargs):
+    def __init__(self, **kwargs):
         super(StatusLabel, self).__init__(**kwargs)
-        self.controller = controller
-        self.controller.serial_conn.bind(last_status=self.update_text)
+        Clock.schedule_interval(self.update_text, 0.1)
 
     i = 0
 
-    def update_text(self, instance, value):
+    def update_text(self, dt):
         self.i += 1
-        self.text = f"{value}: {self.i}"
+        self.text = f"{self.i}"
 
 
 class TestStatusBar(BoxLayout):
     orientation = "horizontal"
 
-    def __init__(self, controller, **kwargs):
+    def __init__(self, **kwargs):
         super(TestStatusBar, self).__init__(**kwargs)
 
-        self.add_widget(StatusLabel(controller))
+        self.add_widget(StatusLabel())
 
 
 class SerialTestApp(App):
 
     def build(self):
-        self.serial = SerialConnection("/dev/ttyS0")
-        self.serial.open()
-        self.controller = SmartBenchController(self.serial)
+        # self.serial = SerialConnection("/dev/ttyS0")
+        # self.controller = SmartBenchController(self.serial)
+        # self.serial.open()
 
-        return TestStatusBar(self.controller)
+        return TestStatusBar()
 
 
 if __name__ == "__main__":
