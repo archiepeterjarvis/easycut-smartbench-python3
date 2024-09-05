428736cf854f0317b7295a632a2e8e04420db6af
Marc <marc.schlotzhauer@yetitool.com>
2024-09-05 21:03:28 +0200

    added setUp function

--- tests_ci/simple_test.py
+++ tests_ci/simple_test.py
@@ -6,9 +6,13 @@ from kivy.app import App
 
 class SimpleTest(unittest.TestCase):
 
-    def test_simple_test(self):
+
+    def setUp(self):
         self._app = App()
         self._app.width = 800
         self._app.height = 480
+        print("Setup PASSED!")
+
+    def test_simple_test(self):
         assert True
         print("TEST PASSED!")
