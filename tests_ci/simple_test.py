afa7d1c36107b17ec133c46bc020768d692398b0
Marc <marc.schlotzhauer@yetitool.com>
2024-09-05 20:43:41 +0200

    not using UnitTestBase anymore

--- tests_ci/simple_test.py
+++ tests_ci/simple_test.py
@@ -1,8 +1,14 @@
-from unit_test_base import UnitTestBase
+import unittest
+
+from kivy.app import App
 
 
-class SimpleTest(UnitTestBase):
+
+class SimpleTest(unittest.TestCase):
 
     def test_simple_test(self):
+        self._app = App()
+        self._app.width = 800
+        self._app.height = 480
         assert True
         print("TEST PASSED!")
