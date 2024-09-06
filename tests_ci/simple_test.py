39acd04a9b7bc35e336e82fcaf55cebcad0b1bd5
Marc <marc.schlotzhauer@yetitool.com>
2024-09-06 11:55:06 +0200

    added unit_test_base.py again

--- tests_ci/simple_test.py
+++ tests_ci/simple_test.py
@@ -1,17 +1,8 @@
-import unittest
-
-from kivy.app import App
+from unit_test_base import UnitTestBase
 
 
+class SimpleTest(UnitTestBase):
 
-class SimpleTest(unittest.TestCase):
-
-
-    def setUp(self):
-        self._app = App()
-        self._app.width = 800
-        self._app.height = 480
-        print("Setup PASSED!")
 
     def test_simple_test(self):
         assert True
